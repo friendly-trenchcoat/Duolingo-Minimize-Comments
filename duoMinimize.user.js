138473208ae4c84168619ff8f78b0c643d199131
friendly-trenchcoat <tippywigwom@yahoo.com>
2016-07-19 18:58:06 -0400

    fixed bug where button disappears upon vote

--- duoMinimize.user.js
+++ duoMinimize.user.js
@@ -1,32 +1,23 @@
 // ==UserScript==
 // @name         Duolingo - Minimize Comments
+// @version      1.2
 // @description  Adds button for each comment which can hide and unhide it and its children
 // @author       friendly-trenchcoat
-// @version      1.0
 // @include      https://www.duolingo.com/*
-// @require	     http://code.jquery.com/jquery-latest.min.js
 // @grant        none
 // ==/UserScript==
 
-var bool = 1;
-setInterval(function() { // check every second
-    // if not in the comments, set to true
-    if(document.body.innerHTML.search('Comments') == -1){
-        bool = 1;
-    }
-    // else if in the comments and true, preform function and set to false
-    else if(document.body.innerHTML.search('Comments') !== -1 && bool == 1){
-        min();
-        bool = 0;
+setInterval(function() { // every second:
+    if(document.body.innerHTML.search('Comments') !== -1){ // if you are in the comments
+        $("li[class='discussion-comments-list-item']").each(function(k,v) {  // for each comment:
+            if( $(v).children().length == 1 ){ // if there is only one child (happens when you vote)
+                var cNum = v.id.match(/comment-(.*)/)[1];  // find comment id number
+                newButton(cNum, v); // and make a button
+            }
+        });
     }
 }, 1000);
 
-function min(){
-    $("li[class='discussion-comments-list-item']").each(function(k,v) {  // for each comment
-        var cNum = v.id.match(/comment-(.*)/)[1];  // find comment id number
-        newButton(cNum, v); // make a button
-    });
-}
 function newButton(cNum, comment){
     var button = $('<button/>', {
         text: "-",
